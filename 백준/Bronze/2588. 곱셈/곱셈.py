e4f60af6ff0d2421c5a03f38367dbbdcd29ccd82
SnowCloudWay <snowcloudway@gmail.com>
2024-09-24 17:50:48 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    b100 = b // 100
+    b10 = b % 100 // 10
+    b1 = b % 10
+
+    print(a * b1)
+    print(a * b10)
+    print(a * b100)
+    print(a * b)
