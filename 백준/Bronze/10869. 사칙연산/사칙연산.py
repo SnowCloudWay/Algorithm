1b3da4b5fcfca058123094c432a76c6f46f7227d
SnowCloudWay <snowcloudway@gmail.com>
2024-09-24 18:13:26 +0900

    [Bronze V] Title: 사칙연산, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    nums = list(map(int, input().split()))
+
+    print(nums[0] + nums[1])
+    print(nums[0] - nums[1])
+    print(nums[0] * nums[1])
+    print(nums[0] // nums[1])
+    print(nums[0] % nums[1])
