6e30a8d6c607ba61ac29cffcb1cfaeda7941f497
SnowCloudWay <snowcloudway@gmail.com>
2024-09-24 18:02:02 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    students = [i for i in range(1, 31)]
+
+    for _ in range(28):
+        applied = int(input())
+        students.remove(applied)
+
+    print(min(students))
+    print(max(students))
