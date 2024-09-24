916d39b2d7838558bb6b4320ad83688d98f2bd32
SnowCloudWay <snowcloudway@gmail.com>
2024-09-24 18:16:28 +0900

    [Bronze V] Title: A+B - 8, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    n = int(input())
+    cnt = 1
+    for i in range(0, n):
+        a, b = map(int, input().split())
+        result = a + b
+        print('Case #{}: {} + {} = {}'.format(cnt, a, b, result))
+        cnt += 1
