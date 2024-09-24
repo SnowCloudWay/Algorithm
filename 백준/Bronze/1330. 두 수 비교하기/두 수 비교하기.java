569656b12db7ee6f48c06218461c7ccdb06e73f6
SnowCloudWay <snowcloudway@gmail.com>
2024-09-24 17:07:18 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 124 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer str = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(str.nextToken());
+		int b = Integer.parseInt(str.nextToken());
+		
+		if(a > b)
+            System.out.println(">");
+        else if(a < b)
+            System.out.println("<");
+        else
+            System.out.println("==");
+		
+	}
+
+}
