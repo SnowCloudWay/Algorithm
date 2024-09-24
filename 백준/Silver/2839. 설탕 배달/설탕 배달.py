b18adced7ea215295520171b75bfe966a834f284
SnowCloudWay <snowcloudway@gmail.com>
2024-09-24 17:54:10 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    num = int(input())
+    count = 0
+
+    while num >= 0:
+        if num % 5 == 0:
+            count += int(num // 5)
+            print(count)
+            break
+
+        num -= 3
+        count += 1
+
+    else:
+        print(-1)
