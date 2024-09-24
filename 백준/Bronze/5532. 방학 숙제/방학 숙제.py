94392ceeb80b54f8610d109cb13f5bbde917c3e4
SnowCloudWay <snowcloudway@gmail.com>
2024-09-24 18:00:44 +0900

    [Bronze IV] Title: 방학 숙제, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5532. 방학 숙제/방학 숙제.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    l = int(input())
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = int(input())
+
+    ac = a // c
+    if a % c != 0:
+        ac += 1
+
+    bd = b // d
+    if b % d != 0:
+        bd += 1
+
+    funDay = l - max(ac, bd)
+    print(funDay)
