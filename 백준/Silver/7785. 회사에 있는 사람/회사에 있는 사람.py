1152d0e94d2fd1603132d9d84227e95da24337d4
SnowCloudWay <snowcloudway@gmail.com>
2024-09-28 21:25:56 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 2580 ms, Memory: 48332 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    n = int(input())
+
+    d = {}
+    for _ in range(n):
+        name, state = map(str, input().split())
+        if state == "enter":
+            d[name] = state
+        else:
+            del d[name]
+
+    d = sorted(d.keys(), reverse=True)
+    for key in d:
+        print(key)
