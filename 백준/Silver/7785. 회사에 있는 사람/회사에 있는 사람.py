88009f53fe7c56d6afda4f2fbc67820f3d133785
SnowCloudWay <snowcloudway@gmail.com>
2024-09-28 21:27:16 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 196 ms, Memory: 48332 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -1,9 +1,11 @@
+import sys
+
 if __name__ == '__main__':
     n = int(input())
 
     d = {}
     for _ in range(n):
-        name, state = map(str, input().split())
+        name, state = map(str, sys.stdin.readline().split())
         if state == "enter":
             d[name] = state
         else:
