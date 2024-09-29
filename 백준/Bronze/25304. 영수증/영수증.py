a88ed90ed509fe61c96b2b0aba250f3252078391
SnowCloudWay <snowcloudway@gmail.com>
2024-09-29 14:24:21 +0900

    [Bronze IV] Title: 영수증, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    X = int(input())
+    N = int(input())
+    total = 0
+
+    for _ in range(N):
+        a, b = map(int, input().split())
+        total += a * b
+
+    if X == total:
+        print("Yes")
+    else:
+        print("No")
