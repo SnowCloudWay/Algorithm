bd7c1596878be02f4195413a562427aad00bae0d
SnowCloudWay <snowcloudway@gmail.com>
2024-09-30 15:18:38 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 212 ms, Memory: 56184 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -0,0 +1,18 @@
+import sys
+
+if __name__ == '__main__':
+    N, M = map(int, sys.stdin.readline().split())
+    d = {}
+
+    for i in range(N):
+        d[str(i+1)] = sys.stdin.readline().strip()
+
+    r_d = dict(map(reversed,d.items()))
+
+    for _ in range(M):
+        s = sys.stdin.readline().strip()
+
+        if s in d.keys():
+            print(d.get(s))
+        else:
+            print(r_d.get(s))
