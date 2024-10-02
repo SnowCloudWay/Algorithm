467876ff0c59bd275bfb1b9e4938fdeffde04d3f
SnowCloudWay <snowcloudway@gmail.com>
2024-10-02 18:11:59 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,25 @@
+import sys
+
+if __name__ == '__main__':
+    while True:
+        l = sys.stdin.readline().rstrip()
+        s = []
+        balance = True
+        if l == '.': break
+        for i in l:
+            if i in '([':
+                s.append(i)
+            elif i == ')':
+                if not s or s[-1] != '(':
+                    balance = False
+                    break
+                s.pop()
+            elif i == ']':
+                if not s or s[-1] != '[':
+                    balance = False
+                    break
+                s.pop()
+        if not s and balance:
+            print("yes")
+        else:
+            print("no")
