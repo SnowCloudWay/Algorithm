00ba56e206ecfe80a584a97b8e13a65be7cb8ef7
SnowCloudWay <snowcloudway@gmail.com>
2024-10-02 17:45:58 +0900

    [Silver IV] Title: 제로, Time: 2368 ms, Memory: 31900 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,11 +1,10 @@
 if __name__ == '__main__':
     K = int(input())
     s = []
-    result = 0
     for _ in range(K):
         num = int(input())
-        if num != 0:
-            s.append(num)
-        elif num == 0:
+        if num == 0:
             s.pop()
+        else:
+            s.append(num)
     print(sum(s))
