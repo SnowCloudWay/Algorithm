915470be0c85aa2374e415f265aa58929a5a300f
SnowCloudWay <snowcloudway@gmail.com>
2024-10-06 17:15:49 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -0,0 +1,10 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().rstrip().split())
+basket = [i for i in range(1, N+1)]
+
+for _ in range(M):
+    i, j = map(int, sys.stdin.readline().rstrip().split())
+    basket[i-1:j] = basket[i-1:j][::-1]
+
+sys.stdout.write(' '.join(map(str, basket)))
