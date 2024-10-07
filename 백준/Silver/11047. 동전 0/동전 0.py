7da7c594d6bccb18ee8081e43055a172a3f74a68
SnowCloudWay <snowcloudway@gmail.com>
2024-10-07 10:14:51 +0900

    [Silver IV] Title: 동전 0, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,16 @@
+import sys
+
+N, K = map(int, sys.stdin.readline().rstrip().split())
+coins = []
+cnt = 0
+
+for _ in range(N):
+    coins.append(int(sys.stdin.readline().rstrip()))
+
+for c in coins[::-1]:
+    if K == 0: break
+    if K // c > 0:
+        cnt += K // c
+        K = K % c
+
+sys.stdout.write(str(cnt))
