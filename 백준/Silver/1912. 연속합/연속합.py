39343bf7b1e6f2c2fc05731e304f2338a3eae99d
SnowCloudWay <snowcloudway@gmail.com>
2024-10-08 17:30:59 +0900

    [Silver II] Title: 연속합, Time: 84 ms, Memory: 38828 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,10 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+num_list = list(map(int, sys.stdin.readline().rstrip().split()))
+dp = [0 for i in range(n)]
+
+for i in range(n):
+    dp[i] = max(dp[i-1] + num_list[i], num_list[i])
+
+sys.stdout.write(str(max(dp)))
