434af311f9932878ce419f04389301f254f16983
SnowCloudWay <snowcloudway@gmail.com>
2024-10-09 15:44:35 +0900

    [Silver I] Title: 카드 구매하기, Time: 104 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -4,8 +4,12 @@ N = int(sys.stdin.readline().rstrip())
 P = [0] + list(map(int, sys.stdin.readline().rstrip().split()))
 dp = [0] * (N + 1)
 
+dp[1] = P[1]
+
 for i in range(1, N+1):
     for j in range(1, i+1):
-        dp[i] = max(dp[i], P[j] + dp[i-j])
+        if dp[i] < dp[i - j] + P[j]:
+            dp[i] = dp[i-j] + P[j]
+        # dp[i] = max(dp[i], P[j] + dp[i-j])
 
 sys.stdout.write(str(dp[N]))
