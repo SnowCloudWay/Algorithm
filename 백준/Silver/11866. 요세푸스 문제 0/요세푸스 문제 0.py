a4da62becf965ae9b91e3f69323c7eeb359117f3
SnowCloudWay <snowcloudway@gmail.com>
2024-10-14 11:06:53 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 52 ms, Memory: 34016 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -0,0 +1,15 @@
+import sys
+from collections import deque
+
+n, k = map(int, sys.stdin.readline().rstrip().split())
+dq = deque(i for i in range(1, n+1))
+
+sys.stdout.write('<')
+
+while n > 1:
+    dq.rotate(-(k-1))
+    sys.stdout.write(str(dq.popleft()))
+    sys.stdout.write(', ')
+    n -= 1
+
+sys.stdout.write(str(dq.popleft()) + '>')
