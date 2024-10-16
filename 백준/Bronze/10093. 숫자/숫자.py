22571b28ce2c94392a05d246140e432b7dd893b4
SnowCloudWay <snowcloudway@gmail.com>
2024-10-16 16:33:55 +0900

    [Bronze II] Title: 숫자, Time: 76 ms, Memory: 36540 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.py
@@ -0,0 +1,14 @@
+import sys
+
+a, b = map(int, sys.stdin.readline().split())
+l = []
+
+if a < b:
+    for i in range(a+1, b):
+        l.append(i)
+else:
+    for i in range(b+1, a):
+        l.append(i)
+
+print(len(l))
+print(*l)
