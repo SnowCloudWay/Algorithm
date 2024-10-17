e6442bff611fc9a9f51b2cc5a9928880a5f9744c
SnowCloudWay <snowcloudway@gmail.com>
2024-10-17 11:42:33 +0900

    [Bronze II] Title: 단어순서 뒤집기, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.py
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.py
@@ -1,7 +1,4 @@
 import sys
-
-n = int(sys.stdin.readline())
-
-for i in range(1, n+1):
+for i in range(1, int(sys.stdin.readline())+1):
     word = list(map(str, sys.stdin.readline().rstrip().split()))
     print('Case #{}:'.format(i), ' '.join(word[::-1]).format(i))
