0a0e83588b0f153c97b7d545bb4149e3e71c79c0
SnowCloudWay <snowcloudway@gmail.com>
2024-10-23 22:02:08 +0900

    [Silver IV] Title: 암기왕, Time: 1040 ms, Memory: 215396 KB -BaekjoonHub

---
+++ 백준/Silver/2776. 암기왕/암기왕.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    s = set(map(int, input().split()))
+    M = int(input())
+    nums = list(map(int, input().split()))
+    for num in nums:
+        if num in s:
+            sys.stdout.write('1\n')
+        else:
+            sys.stdout.write('0\n')
