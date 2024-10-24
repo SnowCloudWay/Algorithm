5ea6679de01e7400a6dcb4b0155e91628360a31e
SnowCloudWay <snowcloudway@gmail.com>
2024-10-24 10:39:18 +0900

    [Silver IV] Title: 수 찾기, Time: 124 ms, Memory: 50532 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,29 +1,13 @@
 import sys
+input = sys.stdin.readline
 
-def binary_search(target, data):
-    start = 0
-    end = len(data) - 1
+n = int(input())
+s = set(map(int, input().split()))
+m = int(input())
+nums = list(map(int, input().split()))
 
-    while start <= end:
-        mid = (start + end) // 2
-
-        if data[mid] == target:
-            return 1
-        elif data[mid] < target:
-            start = mid + 1
-        else:
-            end = mid -1
-
-    return 0
-
-if __name__ == '__main__':
-    n = int(sys.stdin.readline())
-    a = list(map(int, sys.stdin.readline().split()))
-    m = int(sys.stdin.readline())
-    f = list(map(int, sys.stdin.readline().split()))
-
-    a.sort()
-
-    for i in f:
-        result = binary_search(i, a)
-        sys.stdout.write(str(result) + '\n')
+for num in nums:
+    if num in s:
+        sys.stdout.write('1\n')
+    else:
+        sys.stdout.write('0\n')
