02ac6cd1c0cfafaf62cf95d60beab6220d462ff8
SnowCloudWay <snowcloudway@gmail.com>
2024-10-24 18:24:35 +0900

    [Silver III] Title: 두 수의 합, Time: 88 ms, Memory: 42036 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+nums = sorted(list(map(int, input().split())))
+x = int(input())
+start = 0
+end = n - 1
+cnt = 0
+
+while start < end:
+    sum = nums[start] + nums[end]
+
+    if sum == x:
+        cnt += 1
+        start += 1
+    elif sum < x:
+        start += 1
+    else:
+        end -= 1
+
+sys.stdout.write(str(cnt))
