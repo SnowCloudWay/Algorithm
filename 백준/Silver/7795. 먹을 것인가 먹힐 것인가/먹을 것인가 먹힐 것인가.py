78b7c69b847580990b88e967a5525082842be72c
SnowCloudWay <snowcloudway@gmail.com>
2024-10-25 15:59:21 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 140 ms, Memory: 34596 KB -BaekjoonHub

---
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    N, M = map(int, input().split())
+    A = sorted(list(map(int, input().split())))
+    B = sorted(list(map(int, input().split())))
+    a_idx, b_idx = 0, 0
+    cnt = 0
+    while a_idx < N and b_idx < M:
+        if A[a_idx] > B[b_idx]:
+            cnt += N - a_idx
+            b_idx += 1
+        else:
+            a_idx += 1
+
+    sys.stdout.write(str(cnt)+'\n')
