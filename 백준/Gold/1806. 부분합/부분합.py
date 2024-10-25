95d9fc3e7e0395923c22d1fb45e69fdd692c7d43
SnowCloudWay <snowcloudway@gmail.com>
2024-10-25 18:06:02 +0900

    [Gold IV] Title: 부분합, Time: 120 ms, Memory: 42204 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n, s = map(int, input().split())
+nums = list(map(int, input().split()))
+intervalSum = 0
+minLen = n
+check = 0
+end = 0
+
+for start in range(n):
+    while intervalSum < s and end < n:
+        intervalSum += nums[end]
+        end += 1
+    if intervalSum >= s:
+        minLen = min(minLen, end - start)
+        check = 1
+    intervalSum -= nums[start]
+
+if check == 1:
+    sys.stdout.write(str(minLen))
+else:
+    sys.stdout.write(str(0))
