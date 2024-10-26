a5a219dcf7edb924b6adc694ffd0074d5b3efffd
SnowCloudWay <snowcloudway@gmail.com>
2024-10-26 12:27:46 +0900

    [Silver IV] Title: 우당탕탕 영화예매, Time: 2648 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/29700. 우당탕탕 영화예매/우당탕탕 영화예매.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n, m, k = map(int, input().split())
+cnt = 0
+for _ in range(n):
+    seat = [int(i) for i in input().strip()]
+    interval_sum = sum(seat[:k])
+    if interval_sum == 0:
+        cnt += 1
+    start, end = 0, k
+    while end < m:
+        interval_sum -= seat[start]
+        interval_sum += seat[end]
+        start += 1
+        end += 1
+        if interval_sum == 0:
+            cnt += 1
+
+sys.stdout.write(str(cnt))
