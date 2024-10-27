20e755d84a9a89af3e55156d11da98c54d02c5c2
SnowCloudWay <snowcloudway@gmail.com>
2024-10-27 14:43:56 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+d = {'ChongChong':1}
+
+n = int(input())
+for _ in range(n):
+    a, b = map(str, input().split())
+    if a in d == False:
+        d[a] = 0
+    if b in d == False:
+        d[b] = 0
+    if d.get(a) == 1 or d.get(b) == 1:
+        d[a] = 1
+        d[b] = 1
+
+cnt = 0
+for value in d.values():
+    if value == 1:
+        cnt += 1
+
+sys.stdout.write(str(cnt))
