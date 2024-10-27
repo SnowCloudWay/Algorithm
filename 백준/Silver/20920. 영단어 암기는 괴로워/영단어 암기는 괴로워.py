20d4e991530d6b768caa1e0959b623a69ed958be
SnowCloudWay <snowcloudway@gmail.com>
2024-10-27 14:17:35 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 364 ms, Memory: 65468 KB -BaekjoonHub

---
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -0,0 +1,16 @@
+import sys
+from collections import defaultdict
+input = sys.stdin.readline
+
+d = defaultdict(int)
+n, m = map(int, input().split())
+
+for _ in range(n):
+    word = input().strip()
+    if len(word) >= m:
+        d[word] += 1
+
+sorted_d = dict(sorted(d.items(), key=lambda item:(-item[1], -len(item[0]), item[0])))
+
+for word in sorted_d:
+    sys.stdout.write(word+'\n')
