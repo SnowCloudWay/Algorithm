1c1596d048d8a69f49c2791d156d6f8f308313f5
SnowCloudWay <snowcloudway@gmail.com>
2024-10-28 18:37:12 +0900

    [Silver IV] Title: 야근, Time: 96 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/14402. 야근/야근.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+d = {}
+cnt = 0
+
+n = int(input())
+
+for _ in range(n):
+    name, work = input().split()
+    if work == '-':
+        if name not in d or d[name] == 0:
+            cnt += 1
+        else:
+            d[name] -= 1
+    else:
+        if name not in d:
+            d[name] = 1
+        else:
+            d[name] += 1
+
+if len(d) == 0:
+    sys.stdout.write(str(cnt))
+else:
+    sys.stdout.write(str(sum(d.values()) + cnt))
