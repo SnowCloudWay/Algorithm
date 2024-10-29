7b3468ae91dc007c189e69314e311d2573480c76
SnowCloudWay <snowcloudway@gmail.com>
2024-10-29 23:57:49 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 56 ms, Memory: 44704 KB -BaekjoonHub

--- 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.py
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.py
@@ -5,11 +5,12 @@ n, game = input().split()
 s = set(sys.stdin.read().splitlines())
 cnt = 0
 
-if game == 'Y':
-    cnt = len(s)
-elif game == 'F':
-    cnt = len(s) // 2
-else:
-    cnt = len(s) // 3
+match game:
+    case 'Y':
+        cnt = len(s)
+    case 'F':
+        cnt = len(s) // 2
+    case _:
+        cnt = len(s) // 3
 
 sys.stdout.write(str(cnt))
