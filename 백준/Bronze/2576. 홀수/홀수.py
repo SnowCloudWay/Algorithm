fe6c9bb981ac0b1c08a1bd39b8afd738e33decaf
SnowCloudWay <snowcloudway@gmail.com>
2024-10-29 11:14:48 +0900

    [Bronze III] Title: 홀수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2576. 홀수/홀수.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+odd_nums = []
+for _ in range(7):
+    num = int(input())
+    if num % 2 != 0:
+        odd_nums.append(num)
+if len(odd_nums) != 0:
+    sys.stdout.write(str(sum(odd_nums)) + '\n')
+    sys.stdout.write((str(min(odd_nums))))
+else:
+    sys.stdout.write(str(-1))
