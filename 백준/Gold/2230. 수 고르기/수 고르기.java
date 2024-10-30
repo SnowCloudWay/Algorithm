9e9d85c9c454ca4016b837e10c55e1e4da8632ec
SnowCloudWay <snowcloudway@gmail.com>
2024-10-31 00:21:02 +0900

    [Gold V] Title: 수 고르기, Time: 476 ms, Memory: 29136 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        ArrayList<Integer> nums = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            nums.add(Integer.parseInt(br.readLine()));
+        }
+        Collections.sort(nums);
+
+        int answer = Collections.max(nums) - Collections.min(nums);
+        int start = 0;
+        int end = 0;
+
+        while (start < n - 1) {
+            int result = nums.get(end) - nums.get(start);
+            if (result >= m) {
+                if (answer > result) {
+                    answer = result;
+                }
+                start++;
+            } else {
+                if (end < n - 1) {
+                    end++;
+                } else {
+                    start++;
+                }
+            }
+        }
+
+        bw.write(Integer.toString(answer));
+        bw.flush();
+
+    }
+
+}
