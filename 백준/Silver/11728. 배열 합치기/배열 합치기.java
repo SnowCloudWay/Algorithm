8156d50ecdddc8ef11fc822d47d2ff5f03a414ac
SnowCloudWay <snowcloudway@gmail.com>
2024-10-30 15:39:19 +0900

    [Silver V] Title: 배열 합치기, Time: 1560 ms, Memory: 347416 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] num1 = new int[n];
+        int[] num2 = new int[m];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            num1[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            num2[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int idx1 = 0;
+        int idx2 = 0;
+        ArrayList<Integer> nums = new ArrayList<>();
+        while (idx1 < n && idx2 < m) {
+            if (num1[idx1] <= num2[idx2]) {
+                nums.add(num1[idx1++]);
+            } else {
+                nums.add(num2[idx2++]);
+            }
+        }
+        while (idx1 < n) {
+            nums.add(num1[idx1++]);
+        }
+        while (idx2 < m) {
+            nums.add(num2[idx2++]);
+        }
+
+        for (int num : nums) {
+            bw.write(num + " ");
+        }
+        bw.flush();
+
+    }
+
+}
