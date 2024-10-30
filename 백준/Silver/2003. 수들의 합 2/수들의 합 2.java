1a1219d12029d2eb8c14bcf7f20169837440dcc4
SnowCloudWay <snowcloudway@gmail.com>
2024-10-30 12:42:39 +0900

    [Silver IV] Title: 수들의 합 2, Time: 140 ms, Memory: 15084 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] nums = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int intervalSum = 0;
+        int end = 0;
+        int cnt = 0;
+        for (int start = 0; start < n; start++) {
+            while (intervalSum < m && end < n) {
+                intervalSum += nums[end];
+                end += 1;
+            }
+            if (intervalSum == m) {
+                cnt += 1;
+            }
+            intervalSum -= nums[start];
+        }
+
+        bw.write(Integer.toString(cnt));
+        bw.flush();
+
+    }
+
+}
