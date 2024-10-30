0ccd73ebbdd3b60db73c3c080b6f7f8a5719df1d
SnowCloudWay <snowcloudway@gmail.com>
2024-10-30 17:25:37 +0900

    [Silver V] Title: CD, Time: 3592 ms, Memory: 392424 KB -BaekjoonHub

---
+++ 백준/Silver/4158. CD/CD.py
@@ -0,0 +1,20 @@
+import sys
+from collections import defaultdict
+input = sys.stdin.readline
+
+while True:
+    n, m = map(int, input().split())
+    if n == 0 and m == 0:
+        break
+    nums = [input().strip() for _ in range(n + m)]
+    d = defaultdict(int)
+    cnt = 0
+
+    for num in nums:
+        d[num] += 1
+
+    for v in d.values():
+        if v == 2:
+            cnt += 1
+
+    sys.stdout.write(str(cnt)+'\n')
