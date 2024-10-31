e5815def6c7da9dceb0e2848a8191df10d058810
SnowCloudWay <snowcloudway@gmail.com>
2024-10-31 22:16:56 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 56 ms, Memory: 33188 KB -BaekjoonHub

---
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
@@ -0,0 +1,18 @@
+import sys, heapq
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+heap = []
+
+nums = list(map(int, input().split()))
+for num in nums:
+    heapq.heappush(heap, num)
+
+for _ in range(m):
+    a = heapq.heappop(heap)
+    b = heapq.heappop(heap)
+    c = a + b
+    for _ in range(2):
+        heapq.heappush(heap, c)
+
+sys.stdout.write(str(sum(heap)))
