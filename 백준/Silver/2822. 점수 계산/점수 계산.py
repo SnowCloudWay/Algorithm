396dcae197610d46075748c00f528546df735bf3
SnowCloudWay <snowcloudway@gmail.com>
2024-10-31 14:24:52 +0900

    [Silver V] Title: 점수 계산, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2822. 점수 계산/점수 계산.py
@@ -0,0 +1,10 @@
+import sys
+
+scores = list(map(int, sys.stdin.read().splitlines()))
+s_scores = sorted(scores)
+scores_idx = []
+
+sys.stdout.write(str(sum(s_scores[3:]))+'\n')
+for num in s_scores[3:]:
+    scores_idx.append(scores.index(num)+1)
+print(' '.join(map(str, (sorted(scores_idx)))))
