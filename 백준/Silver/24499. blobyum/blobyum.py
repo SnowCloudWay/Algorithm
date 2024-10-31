e048adf224d320f7b9dac2f8ee1fe26dad8ad1a4
SnowCloudWay <snowcloudway@gmail.com>
2024-10-31 22:16:30 +0900

    [Silver IV] Title: blobyum, Time: 112 ms, Memory: 39132 KB -BaekjoonHub

---
+++ 백준/Silver/24499. blobyum/blobyum.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+arr = arr * 2
+
+windowSum = sum(arr[i] for i in range(k))
+maxApplePie = windowSum
+
+for i in range(1, n):
+    windowSum = windowSum - arr[i - 1] + arr[i + k - 1]
+    maxApplePie = max(maxApplePie, windowSum)
+
+sys.stdout.write(str(maxApplePie))
