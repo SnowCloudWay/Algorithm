85a96a2ff08826e00b71d3fc97eeb9acbc139e94
SnowCloudWay <snowcloudway@gmail.com>
2024-10-31 11:25:11 +0900

    [Silver IV] Title: Sort 마스터 배지훈의 후계자, Time: 796 ms, Memory: 77016 KB -BaekjoonHub

---
+++ 백준/Silver/20551. Sort 마스터 배지훈의 후계자/Sort 마스터 배지훈의 후계자.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+def bs_left(target, data):
+    low = 0
+    high = len(data) - 1
+    result = -1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if data[mid] == target:
+            result = mid
+            high = mid - 1
+        elif data[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return result
+
+
+n, m = map(int, input().split())
+nums = list(map(int, sys.stdin.read().splitlines()))
+b = sorted(nums[:n])
+d = nums[n:]
+
+for num in d:
+    sys.stdout.write(str(bs_left(num, b))+'\n')
