0f540c084d7c088c3ca87557597749e9cff399a8
SnowCloudWay <snowcloudway@gmail.com>
2024-11-03 00:49:53 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 104 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -1,26 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException{
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer str = new StringTokenizer(br.readLine());
-		
-		int a = Integer.parseInt(str.nextToken());
-		int b = Integer.parseInt(str.nextToken());
-		
-		if(a > b)
-            System.out.println(">");
-        else if(a < b)
-            System.out.println("<");
-        else
-            System.out.println("==");
-		
-	}
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        if (a > b) bw.write(">");
+        else if (a < b) bw.write("<");
+        else bw.write("==");
+
+        bw.flush();
+
+    }
 
 }
