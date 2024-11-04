1cf622b79d10617c8d2e84ea6a5416f67e695b85
SnowCloudWay <snowcloudway@gmail.com>
2024-11-04 16:22:17 +0900

    [Silver II] Title: N번째 큰 수, Time: 1572 ms, Memory: 33188 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
@@ -0,0 +1,13 @@
+import sys, heapq
+input = sys.stdin.readline
+
+heap = []
+n = int(input())
+for _ in range(n):
+    nums = list(map(int, input().split()))
+    for num in nums:
+        heapq.heappush(heap, num)
+        if len(heap) > n:
+            heapq.heappop(heap)
+
+sys.stdout.write(str(heapq.heappop(heap)))
