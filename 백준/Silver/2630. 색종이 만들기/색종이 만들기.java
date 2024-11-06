164b29a5c158cb3e1ed13bb39ae0c531100314b8
SnowCloudWay <snowcloudway@gmail.com>
2024-11-06 12:11:14 +0900

    [Silver II] Title: 색종이 만들기, Time: 144 ms, Memory: 15624 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int white = 0;
+    static int blue = 0;
+
+    public static void solution(int x, int y, int n, int[][] paper) {
+        int color = paper[x][y];
+        for (int i = x; i < x+n; i++) {
+            for (int j = y; j < y+n; j++) {
+                if (color != paper[i][j]) {
+                    solution(x, y, n/2, paper);
+                    solution(x, y+n/2, n/2, paper);
+                    solution(x+n/2, y, n/2, paper);
+                    solution(x+n/2, y+n/2, n/2, paper);
+                    return;
+                }
+            }
+        }
+        if (color == 0) white += 1;
+        else blue += 1;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] paper = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                paper[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        solution(0, 0, n, paper);
+
+        System.out.println(white);
+        System.out.println(blue);
+
+    }
+
+}
