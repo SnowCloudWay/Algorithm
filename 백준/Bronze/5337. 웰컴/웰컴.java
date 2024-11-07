1ca28b538b8062871bd65f08268167caafe99a19
SnowCloudWay <snowcloudway@gmail.com>
2024-11-07 16:10:51 +0900

    [Bronze V] Title: 웰컴, Time: 96 ms, Memory: 13956 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(".  .   .\n");
+        bw.write("|  | _ | _. _ ._ _  _\n");
+        bw.write("|/\\|(/.|(_.(_)[ | )(/.\n");
+        bw.flush();
+
+    }
+
+}
