c91d5134c87b60fc780bfaa8f2f0a3f6a1c2610c
SnowCloudWay <snowcloudway@gmail.com>
2024-11-09 20:01:42 +0900

    [Silver IV] Title: 서든어택 3, Time: 100 ms, Memory: 42036 KB -BaekjoonHub

---
+++ 백준/Silver/22993. 서든어택 3/서든어택 3.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+aList = list(map(int, input().split()))
+jun = aList[0]
+live = True
+
+aList[1:] = sorted(aList[1:])
+
+for i in range(1, n):
+    a = aList[i]
+    if jun > a:
+        jun += a
+    else:
+        live = False
+        break
+
+print("Yes" if live else "No")
