fa65a0c3ec14b24b5d68a4f2ac1a8b5cb9e03b5f
SnowCloudWay <snowcloudway@gmail.com>
2024-11-09 16:24:58 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+
+s = input().strip()
+
+sys.stdout.write(":fan::fan::fan:\n")
+sys.stdout.write(":fan::" + s + "::fan:\n")
+sys.stdout.write(":fan::fan::fan:")
