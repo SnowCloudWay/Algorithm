d10a4ada19104a5507749abc54e9908629307c4d
SnowCloudWay <snowcloudway@gmail.com>
2024-11-10 18:17:48 +0900

    [Bronze III] Title: 개표, Time: 52 ms, Memory: 34016 KB -BaekjoonHub

---
+++ 백준/Bronze/10102. 개표/개표.py
@@ -0,0 +1,17 @@
+import sys
+from collections import Counter
+input = sys.stdin.readline
+
+v = int(input())
+s = input().strip()
+counter = Counter(s)
+
+cntA = counter["A"]
+cntB = counter["B"]
+
+if cntA > cntB:
+    sys.stdout.write("A")
+elif cntA < cntB:
+    sys.stdout.write("B")
+else:
+    sys.stdout.write("Tie")
