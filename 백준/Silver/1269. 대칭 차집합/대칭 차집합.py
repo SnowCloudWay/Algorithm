1c2eb596501cd47f3c00de5244f3c4c5dbc8a452
SnowCloudWay <snowcloudway@gmail.com>
2024-11-10 15:16:35 +0900

    [Silver IV] Title: 대칭 차집합, Time: 228 ms, Memory: 81104 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+a = set(map(int, input().split()))
+b = set(map(int, input().split()))
+
+sys.stdout.write(str(len(a - b) + len(b - a)))
