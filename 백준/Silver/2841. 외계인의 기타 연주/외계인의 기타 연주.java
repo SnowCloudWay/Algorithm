b4c6ecbc9f5af20c117fdff59cc807a4b792a32d
SnowCloudWay <snowcloudway@gmail.com>
2024-11-11 11:52:28 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 916 ms, Memory: 127336 KB -BaekjoonHub

---
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        Stack<Stack<Integer>> sInt = new Stack<>();
+        for (int i = 0; i < 6; i++)
+            sInt.add(new Stack<>());
+
+        int cnt = 0;
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int p = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            --a;
+            if (sInt.get(a).isEmpty() || sInt.get(a).peek() < b) {
+                sInt.get(a).push(b);
+                cnt++;
+            } else if (sInt.get(a).peek() > b) {
+                while (true) {
+                    if (sInt.get(a).isEmpty() || sInt.get(a).peek() <= b)
+                        break;
+                    sInt.get(a).pop();
+                    cnt++;
+                }
+                if (sInt.get(a).isEmpty() || sInt.get(a).peek() < b) {
+                    sInt.get(a).push(b);
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+
+}
