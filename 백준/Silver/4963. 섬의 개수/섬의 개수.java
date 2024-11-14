ca599fca47532f524ace80cb33408979e91fe8a9
SnowCloudWay <snowcloudway@gmail.com>
2024-11-14 10:38:22 +0900

    [Silver II] Title: 섬의 개수, Time: 144 ms, Memory: 16024 KB -BaekjoonHub

---
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int W, H;
+    static int[][] matrix;
+    static boolean[][] visited;
+
+    static int[] dy = {-1, -1, -1, 1, 1, 1, 0, 0};
+    static int[] dx = {-1, 0, 1, -1, 0, 1, -1, 1};
+
+    static void dfs(int y, int x) {
+        visited[y][x] = true;
+        for (int i = 0; i < 8; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if (0 <= ny && ny < H && 0 <= nx && nx < W) {
+                if(!visited[ny][nx] && matrix[ny][nx] == 1)
+                    dfs(ny, nx);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            W = Integer.parseInt(st.nextToken());
+            H = Integer.parseInt(st.nextToken());
+            if (W == 0 && H == 0) break;
+            matrix = new int[H][W];
+            visited = new boolean[H][W];
+            int cnt = 0;
+
+            for (int i = 0; i < H; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < W; j++) {
+                    matrix[i][j] = Integer.parseInt(st.nextToken());
+                    visited[i][j] = false;
+                }
+            }
+
+            for (int i = 0; i < H; i++) {
+                for (int j = 0; j < W; j++) {
+                    if (!visited[i][j] && matrix[i][j] == 1) {
+                        dfs(i, j);
+                        cnt++;
+                    }
+                }
+            }
+            System.out.println(cnt);
+        }
+
+
+    }
+
+}
