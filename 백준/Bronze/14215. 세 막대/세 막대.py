c62f041fd384b4123064aa20a2bd2283fc3390bd
SnowCloudWay <snowcloudway@gmail.com>
2024-11-21 18:04:23 +0900

    [Bronze III] Title: 세 막대, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+nums = list(map(int, input().split()))
+nums.sort()
+
+if nums[0] + nums[1] <= nums[2]:
+    nums[2] = nums[0] + nums[1] - 1
+
+sys.stdout.write(str(nums[0]+nums[1]+nums[2]))
