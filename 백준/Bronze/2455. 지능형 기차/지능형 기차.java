f7542a2b4f47345848f1a41204baf492f9c720f8
SnowCloudWay <snowcloudway@gmail.com>
2024-11-25 16:17:53 +0900

    [Bronze III] Title: 지능형 기차, Time: 96 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/2455. 지능형 기차/지능형 기차.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int nowPeople = 0, maxPeople = 0;
+        for (int i = 0; i < 4; i++) {
+            st = new StringTokenizer(br.readLine());
+            nowPeople -= Integer.parseInt(st.nextToken());
+            nowPeople += Integer.parseInt(st.nextToken());
+            if (maxPeople < nowPeople)
+                maxPeople = nowPeople;
+        }
+
+        System.out.println(maxPeople);
+
+    }
+}
