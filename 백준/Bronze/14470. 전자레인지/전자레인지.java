e108340917d2a033f1ecae9819c1327dd859e8a5
SnowCloudWay <snowcloudway@gmail.com>
2024-12-25 08:23:29 +0900

    [Bronze IV] Title: 전자레인지, Time: 100 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/14470. 전자레인지/전자레인지.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+        int D = Integer.parseInt(br.readLine());
+        int E = Integer.parseInt(br.readLine());
+        int sec = 0;
+
+        if (A < 0) {
+            sec = (Math.abs(A) * C) + D + (B * E);
+        }
+        else {
+            sec = (B - A) * E;
+        }
+
+        System.out.println(sec);
+
+    }
+
+}
