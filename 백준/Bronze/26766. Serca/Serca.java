9739c9fa0efc12fe3475380befda867a914bf3ad
SnowCloudWay <snowcloudway@gmail.com>
2025-01-18 13:34:09 +0900

    [Bronze V] Title: Serca, Time: 700 ms, Memory: 23936 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(" @@@   @@@ \n" +
+                    "@   @ @   @\n" +
+                    "@    @    @\n" +
+                    "@         @\n" +
+                    " @       @ \n" +
+                    "  @     @  \n" +
+                    "   @   @   \n" +
+                    "    @ @    \n" +
+                    "     @     ");
+        }
+    }
+
+}
